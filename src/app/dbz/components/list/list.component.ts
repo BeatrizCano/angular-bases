cc118f3e725449c9288a2e30cd34cbf191f621ba
Beatriz Cano <130498392+BeatrizCano@users.noreply.github.com>
2024-01-16 13:22:42 +0100

    end_section_5

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,5 +1,6 @@
 
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Character } from "../../interfaces/character.interface";
 
 
 @Component({
@@ -7,4 +8,24 @@ import { Component } from "@angular/core";
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent{}
+export class ListComponent{
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }];
+
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+
+  onDeleteCharacter(id? : string): void {
+
+    if (!id) return;
+   
+    this.onDelete.emit( id);
+  }
+
+}
